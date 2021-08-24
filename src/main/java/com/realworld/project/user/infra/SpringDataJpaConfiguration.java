0f4c8a7b729f0e87e7100098a68cfab3bcdb3c52
bluewow <dogseen@gmail.com>
2021-08-24 23:05:32 +0900

    feat : jpa auditing 추가 infra

---
+++ src/main/java/com/realworld/project/user/infra/SpringDataJpaConfiguration.java
@@ -0,0 +1,9 @@
+package com.realworld.project.user.infra;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class SpringDataJpaConfiguration {
+}
