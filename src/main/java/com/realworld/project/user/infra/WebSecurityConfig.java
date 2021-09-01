aca805245c9a59ff1cec45fd89321fc76435ac38
DAOU-8095\daou <kihyunkim@daou.co.kr>
2021-09-01 16:31:03 +0900

    refactor : rest api server 기 떄문에 csrf disable

--- src/main/java/com/realworld/project/user/infra/WebSecurityConfig.java
+++ src/main/java/com/realworld/project/user/infra/WebSecurityConfig.java
@@ -39,9 +39,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .csrf()
-                .ignoringAntMatchers("/h2-console/**")
-                .disable()
+            .csrf().disable()
             .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 역시 사용하지 않습니다.
             .and()
