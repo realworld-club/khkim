75b13d77a988c25dc1aacbb3e31c99d83001c09f
khkim <kihyunkim@daou.co.kr>
2021-09-29 10:06:10 +0900

    v2 init

--- src/main/java/com/realworld/project/article/domain/ArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.realworld.project.article.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-    void deleteByAuthorId(Long id);
-}
