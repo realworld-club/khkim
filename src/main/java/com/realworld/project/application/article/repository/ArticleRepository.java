09b5105a7f37f55ee7f783f4b1fcac74facaa069
khkim <kihyunkim@daou.co.kr>
2021-10-06 09:00:10 +0900

    refactor : article domain 에 repository package 생성

--- src/main/java/com/realworld/project/application/article/repository/ArticleRepository.java
+++ src/main/java/com/realworld/project/application/article/repository/ArticleRepository.java
@@ -1,5 +1,6 @@
-package com.realworld.project.application.article.domain;
+package com.realworld.project.application.article.repository;
 
+import com.realworld.project.application.article.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
--- src/main/java/com/realworld/project/application/article/repository/TagRepository.java
+++ src/main/java/com/realworld/project/application/article/repository/TagRepository.java
@@ -1,5 +1,6 @@
-package com.realworld.project.application.article.domain;
+package com.realworld.project.application.article.repository;
 
+import com.realworld.project.application.article.domain.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
