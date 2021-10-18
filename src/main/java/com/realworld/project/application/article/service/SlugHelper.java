b301c41a3d7bc79c5fddca0f1c42152981c63d86
khkim <kihyunkim@daou.co.kr>
2021-10-18 09:01:56 +0900

    refactor : slug 요구사항 추가

--- src/main/java/com/realworld/project/application/article/service/SlugHelper.java
+++ src/main/java/com/realworld/project/application/article/service/SlugHelper.java
@@ -1,8 +1,19 @@
 package com.realworld.project.application.article.service;
 
+import java.util.Locale;
+
 public class SlugHelper {
 
+    /**
+     * slug converter
+     *
+     * 대문자는 소문자로
+     * @param text title
+     * @return convert 된 값
+     */
     public static String convert(String text) {
-        return text.replace(" ", "-");
+        text = text.replace(" ", "-");
+        text = text.toLowerCase(Locale.ROOT);
+        return text;
     }
 }
--- src/test/java/com/realworld/project/application/article/service/SlugHelperTest.java
+++ src/test/java/com/realworld/project/application/article/service/SlugHelperTest.java
@@ -2,6 +2,7 @@ package com.realworld.project.application.article.service;
 
 import com.realworld.project.fixture.ArticleFixture;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.realworld.project.fixture.ArticleFixture.slug;
@@ -11,11 +12,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SlugHelperTest {
 
+    @DisplayName("- 추가 테스트")
     @Test
-    void convert() {
+    void convert_case1() {
         String convert = SlugHelper.convert(title);
 
         assertThat(convert).isEqualTo(slug);
+    }
 
+    @DisplayName("toLowerCase 테스트")
+    @Test
+    void convert_case2() {
+        String convert = SlugHelper.convert("Article Test");
+
+        assertThat(convert).isEqualTo("article-test");
     }
 }
