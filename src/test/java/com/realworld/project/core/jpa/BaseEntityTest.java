d19cf632a1e089b888b70da3e1390b9917af8e16
khkim <kihyunkim@daou.co.kr>
2021-11-02 17:00:19 +0900

    feat: batch_size 추가, test class transactional annotation 추가

--- src/test/java/com/realworld/project/core/jpa/BaseEntityTest.java
+++ src/test/java/com/realworld/project/core/jpa/BaseEntityTest.java
@@ -12,12 +12,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import static com.realworld.project.fixture.ArticleFixture.*;
 import static com.realworld.project.fixture.UserFixture.makeUser;
 import static org.assertj.core.api.Assertions.*;
 
 @ActiveProfiles("test")
+@Transactional
 @SpringBootTest
 class BaseEntityTest {
 
